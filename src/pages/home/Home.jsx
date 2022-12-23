6052041d5ef23ec74bac1191c936bd577181aa3e
nattygold <natty@gmail.com>
2022-12-23 05:16:24 -1200

    Initial commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Topbar from '../../components/topbar/Topbar'
+import Sidebar from '../../components/sidebar/Sidebar'
+
+export default function Home() {
+  return (
+    <>    
+      <Topbar/>
+      <Sidebar/>
+
+    </>
+  )
+}
